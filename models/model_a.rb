6b82660f8ac001e9c79e3c4b74e2f6cfec359731
Sherman Au <shermau13@gmail.com>
2021-12-17 16:32:20 -0700

    Add hash model of dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  common_name: 'Velociraptor',
+  genus: 'dromaeosaurid theropod dinosaur',
+  avg_height_in_meters: 2.07,
+  avg_weight_in_kilograms: 15,
+  is_carnivore: true,
+  is_herbivore: false,
+  predatory_behaviors: ["nocturnal", "scavenger", "disembowel"],
+  closely_resembles: ["eagles", "hawks", "owls"]
+}

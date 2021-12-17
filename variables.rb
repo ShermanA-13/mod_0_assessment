6609ccb6b89da010a816a2990703fd14766c53f8
Sherman Au <shermau13@gmail.com>
2021-12-17 16:14:54 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+movie = "Django Unchained"
+is_entertaining = true
+
+movie = "Scary Movie"
+
+num_4 = 4
+num_6 = 2
+
+@product = num_4 * num_6
+
+favorite_foods = ["Cheese", "Bao Buns", "Ice Cream"]
+favorite_foods.shift
+
+num_chars_movie =  p movie.bytesize

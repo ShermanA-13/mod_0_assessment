2c66fbe11e61269d6a5cf1777a620e5d78277d60
Sherman Au <shermau13@gmail.com>
2021-12-17 16:40:06 -0700

    Add hash model of developer

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  first_name: 'Sherman',
+  occupation: 'Cook/Student',
+  current_age: 29,
+  height_in_cm: 172.72,
+  is_tired: true,
+  is_caffinated: false,
+  hobbies: ["rock climbing", "video games", "D&D", "crafting"],
+  dislikes: ["slow walkers", "bigots", "last minute changes", "lack of ownership"]
+}
